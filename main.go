a14e2bdbdc3938f7a35b8f2bf74a1cce0f845dd8
Stovenn <stvndev93@gmail.com>
2025-09-20 11:44:05 +0200

    script refactor

--- main.go
+++ main.go
@@ -1,52 +1,103 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"io"
 	"io/fs"
-	"log"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 func main() {
-	workingDirectory := os.Args[1]
-
-	entries, err := os.ReadDir(workingDirectory)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	for _, entry := range entries {
-		if entry.IsDir() {
-			continue
-		}
-		extensionStart := strings.LastIndex(entry.Name(), ".")
-		extension := strings.ToLower(entry.Name()[extensionStart+1:])
-
-		checkIfDirExist(workingDirectory, extension)
-
-		targetDirectory := workingDirectory + "/" + extension
-
-		if !fileExistsInTargetDirectory(targetDirectory, entry.Name()) {
-			err = os.Rename(workingDirectory+"/"+entry.Name(), targetDirectory+"/"+entry.Name())
-			if err != nil {
-				log.Fatalln(err)
-			}
-		}
-	}
-}
-
-func checkIfDirExist(workingDirectory, dirName string) {
-	if _, err := os.Stat(dirName); os.IsNotExist(err) {
-		os.Mkdir(workingDirectory+"/"+dirName, fs.FileMode(0700))
-	} else {
+	args := os.Args
+	if len(args) == 1 {
+		fmt.Println("You should provide a path")
 		return
 	}
+
+	workDir, err := filepath.Abs(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	filesByExtMap := make(map[string][]string)
+	err = filepath.Walk(workDir, func(path string, info fs.FileInfo, err error) error {
+		if info.IsDir() && (strings.HasPrefix(info.Name(), ".") || path != workDir) {
+			return filepath.SkipDir
+		}
+		if info.IsDir() {
+			return err
+		}
+		path = strings.TrimPrefix(path, workDir)
+		if strings.HasPrefix(path, ".") {
+			return err
+		}
+		parts := strings.SplitN(info.Name(), ".", 2)
+		var ext string
+		if len(parts) != 2 {
+			ext = "other"
+		} else {
+			ext = parts[1]
+		}
+		filesByExtMap[ext] = append(filesByExtMap[ext], path)
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(filesByExtMap)
+
+	for ext, paths := range filesByExtMap {
+		extensionFolder := filepath.Join(workDir, ext)
+		_, err := os.Stat(extensionFolder)
+		if err != nil && os.IsNotExist(err) {
+			err = os.Mkdir(extensionFolder, fs.FileMode(0755))
+		}
+		if err == nil {
+			err = moveFiles(paths, workDir, extensionFolder)
+		}
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
 }
 
-func fileExistsInTargetDirectory(targetDirectory, fileName string) bool {
-	if _, err := os.Stat(targetDirectory + "/" + fileName); os.IsExist(err) {
-		return true
-	} else {
-		return false
+func moveFiles(paths []string, srcDir string, destDir string) error {
+	for _, path := range paths {
+		f, err := os.OpenFile(filepath.Join(srcDir, path), os.O_RDONLY, 0655)
+		if err != nil {
+			return err
+		}
+
+		destPath := filepath.Join(destDir, path)
+		stat, err := os.Stat(destPath)
+		if err != nil && !os.IsNotExist(err) {
+			return err
+		}
+
+		// the file exists check its size to know if it's safe to copy
+		if stat != nil && stat.Size() > 0 {
+			fmt.Printf("[WARNING]: '%s' already exists do you want to overwrite it ? [y/n]\n", destPath)
+			sc := bufio.NewScanner(os.Stdin)
+			_ = sc.Scan()
+			if !strings.EqualFold(sc.Text(), "y") {
+				fmt.Printf("skipping '%s'...\n", destPath)
+				continue
+			}
+		}
+		dst, err := os.OpenFile(destPath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0655)
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(dst, f)
+		if err != nil {
+			return err
+		}
 	}
+	return nil
 }
